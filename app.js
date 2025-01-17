68bf0989832943cb865b2efadf2738c6a8d091e9
SatyamTechie010 <sonysahoo548@gmail.com>
2025-01-17 19:40:52 +0530

    Modified

--- app.js
+++ app.js
@@ -41,15 +41,11 @@ app.get("/BackendAddPlace",async(req,res,next)=>{
     res.render("index.ejs",{results})
  
 }) 
-<<<<<<< Updated upstream
- 
-=======
 class name {
     constructor(parameters) {
         
     }
 }
->>>>>>> Stashed changes
 
 app.get("/addDb",(req,res,next)=>{ 
     res.render("placeDb.ejs")
@@ -97,11 +93,7 @@ app.put("/mysticOdisha/:id",async(req,res,next)=>{
 app.delete("/place/:id",async(req,res,next)=>{
     let {id}=req.params;
     await Place.findByIdAndDelete(id)
-<<<<<<< Updated upstream
-    res.redirect("/BackendAddPlace")
-=======
     res.redirect("/") 
->>>>>>> Stashed changes
 })
 app.listen(3030,()=>{
     console.log("app listing on port 3030");
